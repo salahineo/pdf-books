9e931451d5ada4a8a4c40c140ea4457dc84094f3
Mohamed Salah <salahineo.personal@gmail.com>
2020-12-23 09:58:58 +0200

    Front-End Design Only

---
+++ Front-End/dashboard/js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+  $("#menu-toggle").click(function (e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+  });
+  $(".confirm").click(function () {
+    return confirm("هل أنت متأكد ؟");
+  });
+
+});
